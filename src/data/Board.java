d3db870868b229bb694094646e48d46df67a9044
brian.e.fish <brian.e.fish@d999ffd3-c843-0410-a4e3-63299a4b5887>
2008-02-05 21:27:16 +0000

    Piece.java, Board.java, BoardAnalyzer.java, and related tests updated per discussion
    Open issue: placement currently assumes reference row/colum is the upper left hand corner of the piece.  How to ensure or modify for players starting in other corners?

--- src/data/Board.java
+++ src/data/Board.java
@@ -15,8 +15,13 @@ public class Board {
 	/** 
 	 * Creates an empty board.
 	 */
-	public Board() {
+	public Board()
+	{
 		_blocks = new int[Y_DIMENSION][X_DIMENSION];
+		_blocks[0][0] = 5;
+		_blocks[0][19] = 6;
+		_blocks[19][19] = 7;
+		_blocks[19][0] = 8;
 	}
 	
 	public synchronized int getBlock(int row, int col) {
@@ -28,6 +33,10 @@ public class Board {
 	 */
 	public synchronized void reset() {
 		_blocks = new int[Y_DIMENSION][X_DIMENSION];
+		_blocks[0][0] = 5;
+		_blocks[0][19] = 6;
+		_blocks[19][19] = 7;
+		_blocks[19][0] = 8;
 	}
 	
 	/**
@@ -36,13 +45,31 @@ public class Board {
 	 * @param piece Blokus piece to place.
 	 * @param index Player index.
 	 */
-	public synchronized void place(Piece piece, int row, int col, int index) {
-		
-		if (BoardAnalyzer.canPlace(this, piece, row, col, index)) {
-			for (int i=0; i < piece.getHeight(); i++) {
-				for (int j=0; j < piece.getWidth(); j++) {
-					if (piece.hasBlock(i, j)) {
-						_blocks[row+i][col+j] = index;
+	public synchronized void place(Piece piece, int row, int col, int index)
+	{
+		int bRow, bCol;
+		if (BoardAnalyzer.canPlace(this, piece, row - 1, col - 1, index))
+		{
+			int placement = 0;
+			for (int i=0; i < piece.getHeight(); i++)
+			{
+				bRow = row + i - 1;
+				for (int j=0; j < piece.getWidth(); j++) 
+				{
+					bCol = col + j - 1;
+					if (bRow >= 0 && bCol >= 0 && piece.hasBlock(i, j) && 
+							(_blocks[bRow][bCol] == 0 || _blocks[bRow][bCol] == (index + 4))) 
+					{
+						placement = piece.getBlockType(i, j);
+						if (placement == 3)
+						{
+							placement = 4;
+						}
+						else
+						{
+							placement = 0;
+						}
+						_blocks[bRow][bCol] = placement + index;
 					}
 				}
 			}
--- src/data/Hand.java
+++ src/data/Hand.java
@@ -1,16 +1,46 @@
 package data;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class Hand {
+	
+	private ArrayList<Piece> aHand = new ArrayList<Piece>();
 
 	/**
 	 * Creates an instance of a Hand.
 	 * 
 	 */
-	public Hand() {
+	public Hand() 
+	{
+		reset();
+		addAllNewPieces();
 	}
 	
-	public void reset() {
-		
+	public void reset()
+	{
+		aHand.clear();	
 	}
+	
+	public int piecesLeft()
+	{
+		return aHand.size();
+	}
+	
+	public Iterator getIterator()
+	{
+		return aHand.iterator();
+	}
+	
+	private void addAllNewPieces()
+	{
+		Piece.Type[] types = Piece.Type.values();
+		for (int i = 0; i < types.length; i++)
+		{
+			aHand.add(new Piece(types[i]));
+		}
+	}
+	
+	
 }
